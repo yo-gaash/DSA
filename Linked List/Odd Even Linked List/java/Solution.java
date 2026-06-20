28cbb5f58d057689c190d19082db8194d77b33ac
YOGASH S <159635469+yo-gaash@users.noreply.github.com>
2026-06-20 14:36:45 +0530

    feat: Add Odd Even Linked List

--- Linked List/Odd Even Linked List/java/Solution.java
+++ Linked List/Odd Even Linked List/java/Solution.java
@@ -10,6 +10,7 @@
  */
 class Solution {
     public ListNode oddEvenList(ListNode head) {
+        if(head == null || head.next == null) return head;
         ListNode odd = head;
         ListNode even = head.next;
         ListNode evenHead = even;
@@ -18,7 +19,6 @@ class Solution {
             odd = odd.next;
             even.next = odd.next;
             even = even.next;
-
         }
         odd.next = evenHead;
         return head;
