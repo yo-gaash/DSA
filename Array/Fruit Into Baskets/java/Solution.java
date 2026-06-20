6b142c4f053582e3b249683c61772a15be236778
YOGASH S <159635469+yo-gaash@users.noreply.github.com>
2026-06-21 00:46:37 +0530

    feat: Add Fruit Into Baskets

--- Array/Fruit Into Baskets/java/Solution.java
+++ Array/Fruit Into Baskets/java/Solution.java
@@ -1,22 +1,16 @@
 class Solution {
     public int totalFruit(int[] fruits) {
-        Map<Integer, Integer> bucket = new LinkedHashMap<>();
+        Map<Integer, Integer> bucket = new HashMap<>();
         int left = 0;
         int max = Integer.MIN_VALUE;
         for(int right=0;right<=fruits.length-1;right++){
             bucket.put(fruits[right],bucket.getOrDefault(fruits[right],0)+1);
-            for(Map.Entry<Integer,Integer> entrySet : bucket.entrySet()){
-                System.out.print(entrySet.getKey()+"->"+entrySet.getValue());
-            }
             while(bucket.size()>2){
                 bucket.put(fruits[left],bucket.get(fruits[left])-1);
                 if(bucket.get(fruits[left])==0){
                     bucket.remove(fruits[left]);
                 }
                 left++;
-            }
-            for(Map.Entry<Integer,Integer> entrySet : bucket.entrySet()){
-                System.out.print(entrySet.getKey()+"->"+entrySet.getValue());
             }
             max = Math.max(max,right-left+1);
         }
