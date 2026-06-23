d08e54795404783db15ac8e8581fa5dbaac84f83
YOGASH S <159635469+yo-gaash@users.noreply.github.com>
2026-06-23 18:55:26 +0530

    feat: Add Apply Operations to an Array

--- Array/Apply Operations to an Array/java/Solution.java
+++ Array/Apply Operations to an Array/java/Solution.java
@@ -1,26 +1,17 @@
 class Solution {
     public int[] applyOperations(int[] nums) {
-int i,j,n = nums.length;
-        for(i=0;i<n-1;i++){
-            if(nums[i] == nums[i+1]){
+        int i,j,n = nums.length;
+        for(i=0,j=i+1;i<n-1;i++,j++){
+            if(nums[i] == nums[j]){
                 nums[i] = nums[i]*2;
-                nums[i+1] = 0;
+                nums[j] = 0;
             }
         }
-        i=0;
-        j=i+1;
-        while(j<n){
-            if(nums[i]==0 && nums[j]!=0){
-                nums[i] = nums[j];
-                nums[j] = 0;
-                i++;
-                j++;
-            }
-            else if(nums[i]==0 && nums[j]==0){
-                j++;
-            }
-            else {
-                i++;
+        for(i=0;i<n-1;i++){
+            j=i;
+            if(nums[j]==0 && nums[i+1]!=0){
+                nums[j] = nums[j+1];
+                nums[j+1] = 0;
                 j++;
             }
         }
