cf2e58c337d44f0eb85843cb003c5ecd2c1b332f
YOGASH S <159635469+yo-gaash@users.noreply.github.com>
2026-06-25 16:02:00 +0530

    feat: Add Product of Array Except Self

--- Array/Product of Array Except Self/java/Solution.java
+++ Array/Product of Array Except Self/java/Solution.java
@@ -1,20 +1,19 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int[] prefix = new int[nums.length];
-        for(int i=0;i<=nums.length-1;i++){
-            int prod = 1;
-            for(int j=0;j<=nums.length-1;j++){
-                if(i==j){
-                    continue;
-                }
-                else{
-                    prod = prod*nums[j];
-                    System.out.println(prod+" "+j);
-                }
-            }
-            //System.out.print(prod);
-            prefix[i] = prod;
+        int[] answer = new int[nums.length];
+        int[] prefixProd = new int[nums.length];
+        int suffix[] = new int[nums.length];
+        Arrays.fill(prefixProd,1);
+        Arrays.fill(suffix,1);
+        
+        for(int i=1;i < nums.length;i++){
+            prefixProd[i] = prefixProd[i-1]*nums[i-1]; // 1 1 2 6
         }
-        return prefix;
+        for(int i = nums.length-2 ; i >= 0 ; i--){
+            suffix[i] = suffix[i+1] * nums[i+1];
+        }
+        for(int i = 0 ; i < nums.length ; i++) nums[i] = prefixProd[i] * suffix[i];
+
+        return nums;
     }
 }
