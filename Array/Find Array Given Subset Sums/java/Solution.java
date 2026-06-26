a471c18e116b4ea414e69a930c648220d785b872
YOGASH S <159635469+yo-gaash@users.noreply.github.com>
2026-06-26 15:23:37 +0530

    feat: Add Find Array Given Subset Sums

--- Array/Find Array Given Subset Sums/java/Solution.java
+++ Array/Find Array Given Subset Sums/java/Solution.java
@@ -1,46 +1,49 @@
 class Solution {
-    public int[] recoverArray(int n, int[] sums) {
-        List<Integer> list = new ArrayList<>();
-        Arrays.sort(sums);
-        for(int num : sums){
-            list.add(num);
-        }
-        List<Integer> res = new ArrayList<>();
-        res.add(recurse(list));
+    public static int[] recoverArray(int n, int[] sums) {
         int[] ans = new int[n];
-        for(int i=0;i<=res.size()-1;i++){
-            ans[0] = res.get(i);
-        }
+        recurse(sums,n,ans,0);
+        System.out.print(Arrays.toString(ans));
         return ans;
     }
-    public static int recurse(List<Integer> list){
-        List<Integer> groupA = new ArrayList<>();
-        List<Integer> groupB = new ArrayList<>();
-        int diff = list.get(list.size()-1)-list.get(list.size()-2);
-        if(groupA.size()==1||groupB.size()==1){
-            if(groupA.contains(0)){
-                return diff;
-            }
-            else{
-                return -1*diff;
-            }
+    public static int[] recurse(int[] sums,int n, int[] ans,int index){
+        if(n==0){
+            return ans;
         }
-        List<Integer> ans = new ArrayList<>();
-        int smallest = list.get(0);
-        int pair = diff-smallest;
-        groupA.add(smallest);
-        groupB.add(pair);
-        list.remove(Integer.valueOf(smallest));
-        list.remove(Integer.valueOf(pair));
-        System.out.print(groupA);
-        System.out.print(groupB);
-        if(groupA.contains(0)){
-            ans.add(recurse(groupA));
+        Arrays.sort(sums);
+        int diff = sums[1]-sums[0];
+        int[] groupA = new int[sums.length/2];
+        int[] groupB = new int[sums.length/2];
+        int idxA = 0;
+        int idxB = 0;
+        Map<Integer,Integer> count = new HashMap<>();
+        for(int num:sums){
+            count.put(num,count.getOrDefault(num,0)+1);
+        }
+        for(int num:sums){
+            if(count.getOrDefault(num,0)==0){
+                continue;
+            }
+            count.put(num,count.get(num)-1);
+            count.put(num+diff,count.get(num+diff)-1);
+            groupA[idxA++] = num;
+            groupB[idxB++] = num+diff;
+        }
+        if(containsZero(groupA)){
+            ans[index] = diff;
+            return recurse(groupA,n-1,ans,index+1);
         }
         else{
-            ans.add(recurse(groupB));
-            diff = -diff;
+            ans[index] = -diff;
+            return recurse(groupB,n-1,ans,index+1);
         }
-        return diff;
+
+    }
+    public static boolean containsZero(int[] arr){
+        for(int num:arr){
+            if(num==0){
+                return true;
+            }
+        }
+        return false;
     }
 }
