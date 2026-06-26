5d018fe62c22d09f269aa53aac4422413bba955e
YOGASH S <159635469+yo-gaash@users.noreply.github.com>
2026-06-26 17:13:47 +0530

    feat: Add Rotate List

--- Linked List/Rotate List/java/Solution.java
+++ Linked List/Rotate List/java/Solution.java
@@ -10,7 +10,7 @@
  */
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
-        if(head == null){
+        if(head == null||head.next==null){
             return null;
         }
         ListNode temp = head;
