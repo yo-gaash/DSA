ed072c34b7f59d50bf9d4c8bcffdb00d52ddf63f
YOGASH S <159635469+yo-gaash@users.noreply.github.com>
2026-06-27 16:39:54 +0530

    feat: Add Degree of an Array

--- Array/Degree of an Array/java/Solution.java
+++ Array/Degree of an Array/java/Solution.java
@@ -1,56 +1,25 @@
 class Solution {
     public int findShortestSubArray(int[] nums) {
-        if(nums.length==1){
-            return 1;
-        }
-        Map<Integer,Integer> frequency = new HashMap<>();
-        for(int num:nums){
-            frequency.put(num,frequency.getOrDefault(num,0)+1);
-        }
-        int max = Integer.MIN_VALUE;
-        for(Map.Entry<Integer,Integer> entry:frequency.entrySet()){
-            max = Math.max(max,entry.getValue());
-        } 
-        int i = 0;
-        for(Map.Entry<Integer,Integer> entry : frequency.entrySet()){
-            if(entry.getValue()==max){
-                i++;
-            }
-        }      
-        int[] keys = new int[i];
-        int idx = 0;
-        for(Map.Entry<Integer,Integer> entry:frequency.entrySet()){
-            if(entry.getValue()==max){
-                keys[idx] = entry.getKey();
-                idx++;
-            }
-        }
-        System.out.println(Arrays.toString(keys));
+        int freq[] = new int[50001];
+        int index[] = new int[50001];
+        boolean visited[] = new boolean[50001];
+        int max =0;
         int min = Integer.MAX_VALUE;
-        for(int key:keys){
-            int count = 0;
-            int start = 0;
-            int end = 0;
-            int minLen = Integer.MAX_VALUE;
-            while(end<=nums.length-1){
-                if(nums[end]==key){
-                    count++;
-                }
-                while(count==max){
-                    if(nums[start]==key){
-                        minLen = Math.min(minLen,end-start+1);
-                        System.out.println(key+" "+minLen);
-                        break;
-                    }
-                    start++;
-                }
-                if(count==max){
-                    break;
-                }
-                end++;
-            }
-            min = Math.min(minLen,min);
+        for(int i : nums){
+            freq[i]++;
+            max = Math.max(max,freq[i]);
         }
+        for(int i = 0 ; i < nums.length ; i++){
+            if(freq[nums[i]] == max) index[nums[i]] = i;
+        }
+        for(int i = 0 ; i < nums.length ; i++){
+            if(freq[nums[i]] == max && !visited[nums[i]]){
+                min = Math.min(min, index[nums[i]]-i+1);
+                visited[nums[i]] = true;
+            }
+        }
+        if(min == Integer.MAX_VALUE) return 1;
         return min;
+
     }
 }
