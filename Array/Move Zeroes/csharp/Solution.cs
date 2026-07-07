436933965042428b8886823f4c80cfb699b91db9
YOGASH S <159635469+yo-gaash@users.noreply.github.com>
2026-07-07 12:56:31 +0530

    feat: Add Move Zeroes

---
+++ Array/Move Zeroes/csharp/Solution.cs
@@ -0,0 +1,28 @@
+public class Solution {
+    public void MoveZeroes(int[] nums) {
+        int zeroIdx = -1;
+        for(int i=0;i<=nums.Length-1;i++){
+            if(nums[i]==0){
+                zeroIdx = i;
+                break;
+            }
+        }
+        Console.WriteLine(zeroIdx);
+        int j = zeroIdx+1;
+        while(j<=nums.Length-1){
+            if(nums[j]!=0){
+                swap(zeroIdx,j,nums);
+                zeroIdx++;
+                j++;
+            }
+            else{
+                j++;
+            }
+        }
+    }
+    public static void swap(int idx1, int idx2, int[] nums){
+        int temp = nums[idx1];
+        nums[idx1] = nums[idx2];
+        nums[idx2] = temp;
+    }
+}
--- README.md
+++ README.md
@@ -28,6 +28,7 @@ A professionally structured collection of topic-wise DSA solutions, optimized co
 | 88 | [Merge Sorted Array](./Array/Merge%20Sorted%20Array/) | Easy |
 | 209 | [Minimum Size Subarray Sum](./Array/Minimum%20Size%20Subarray%20Sum/) | Medium |
 | 238 | [Product of Array Except Self](./Array/Product%20of%20Array%20Except%20Self/) | Medium |
+| 283 | [Move Zeroes](./Array/Move%20Zeroes/) | Easy |
 | 525 | [Contiguous Array](./Array/Contiguous%20Array/) | Medium |
 | 697 | [Degree of an Array](./Array/Degree%20of%20an%20Array/) | Easy |
 | 904 | [Fruit Into Baskets](./Array/Fruit%20Into%20Baskets/) | Medium |
