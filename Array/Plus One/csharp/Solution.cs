c268300c91341c3c505206241a48885a9991bd41
YOGASH S <159635469+yo-gaash@users.noreply.github.com>
2026-07-07 15:31:51 +0530

    feat: Add Plus One

--- Array/Plus One/csharp/Solution.cs
+++ Array/Plus One/csharp/Solution.cs
@@ -1,12 +1,12 @@
 public class Solution {
     public int[] PlusOne(int[] digits) {
-        int num = 0;
+        long num = 0;
         for(int i=0;i<=digits.Length-1;i++){
             num = (num*10)+digits[i];
         }
         Console.WriteLine(num);
         num = num+1;
-        int temp = num;
+        long temp = num;
         int length = 0;
         while(temp!=0){
             temp = temp/10;
@@ -15,7 +15,7 @@ public class Solution {
         Console.WriteLine(length);
         int[] result = new int[length];
         for(int i=length-1;i>=0;i--){
-            result[i] = num%10;
+            result[i] = Convert.ToInt32(num%10);
             num = num/10;
         } 
         return result;
