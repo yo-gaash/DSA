629f4263c6b1a4695c9987eab6df9bb43db8d7dd
YOGASH S <159635469+yo-gaash@users.noreply.github.com>
2026-07-07 15:18:39 +0530

    feat: Add Move Zeroes

--- Array/Move Zeroes/csharp/Solution.cs
+++ Array/Move Zeroes/csharp/Solution.cs
@@ -1,11 +1,14 @@
 public class Solution {
     public void MoveZeroes(int[] nums) {
-        int zeroIdx = 0;
+        int zeroIdx = -1;
         for(int i=0;i<=nums.Length-1;i++){
             if(nums[i]==0){
                 zeroIdx = i;
                 break;
             }
+        }
+        if(zeroIdx == -1){
+            return;
         }
         Console.WriteLine(zeroIdx);
         int j = zeroIdx+1;
