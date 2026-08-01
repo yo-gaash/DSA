7641c52b16d12fdb71edd537c2d06dd0e7644fa7
YOGASH S <159635469+yo-gaash@users.noreply.github.com>
2026-08-01 15:31:44 +0530

    feat: Add Single Number

--- LeetCode/Array/Single Number/java/Solution.java
+++ LeetCode/Array/Single Number/java/Solution.java
@@ -1,8 +1,8 @@
 class Solution {
     public int singleNumber(int[] nums) {
         int xor = 0;
-        for(int num : nums){
-            xor = xor ^ num;
+        for(int num:nums){
+            xor = xor^num;
         }
         return xor;
     }
