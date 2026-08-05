4011889a7e03f1889a2dc0dfa8a0f06e2083f4fa
YOGASH S <159635469+yo-gaash@users.noreply.github.com>
2026-08-05 14:50:04 +0530

    feat: Add Best Time to Buy and Sell Stock

--- LeetCode/Array/Best Time to Buy and Sell Stock/java/Solution.java
+++ LeetCode/Array/Best Time to Buy and Sell Stock/java/Solution.java
@@ -1,16 +1,15 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int buyPrice = prices[0];
-        int maxProfit = 0;
-        for(int i=1;i<prices.length;i++){
-            if(prices[i]<buyPrice){
-                buyPrice = prices[i];
+        int buyPrice = Integer.MAX_VALUE;
+        int profit = 0;
+        for(int i=0;i<=prices.length-2;i++){
+            buyPrice = Math.min(buyPrice,prices[i]);
+            int sellPrice = 0;
+            if(prices[i+1]>buyPrice){
+                sellPrice = prices[i+1];
             }
-            int currProfit = prices[i]-buyPrice;
-            if(currProfit>maxProfit){
-                maxProfit = currProfit;
-            }
+            profit = Math.max(profit,sellPrice-buyPrice);
         }
-        return maxProfit;
+        return profit;
     }
 }
