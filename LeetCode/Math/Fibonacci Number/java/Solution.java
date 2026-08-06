a1d3807e893c5e60515103f0b43a04676f94dbd3
YOGASH S <159635469+yo-gaash@users.noreply.github.com>
2026-08-06 12:17:39 +0530

    feat: Add Fibonacci Number

--- LeetCode/Math/Fibonacci Number/java/Solution.java
+++ LeetCode/Math/Fibonacci Number/java/Solution.java
@@ -1,17 +1,11 @@
 class Solution {
     public int fib(int n) {
-        int[] dp = new int[n+1];
-        Arrays.fill(dp,-1);
-        return helper(n, dp);
-        
-    }
-    public int helper(int n , int[]dp){
-
-        if(n<=1) return n;
-
-        if(dp[n] != -1) return dp[n];
-
-        dp[n] = helper(n-1,dp)+ helper(n-2, dp);
-        return dp[n];
+        if(n==0){
+            return 0;
+        }
+        if(n==1){
+            return 1;
+        }
+        return fib(n-1)+fib(n-2);
     }
 }
