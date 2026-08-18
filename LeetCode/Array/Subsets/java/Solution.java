d0c06147217e86975822e0ea90131b19ddca28b0
YOGASH S <159635469+yo-gaash@users.noreply.github.com>
2026-08-18 10:15:34 +0530

    feat: Add Subsets

--- LeetCode/Array/Subsets/java/Solution.java
+++ LeetCode/Array/Subsets/java/Solution.java
@@ -1,25 +1,15 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> ans = subset(new ArrayList<>(), nums);
-        return ans;
-    }
-    public static List<List<Integer>> subset(List<Integer> p, int[] up){
-        if(up.length==0){
-            List<List<Integer>> list = new ArrayList<>();
-            list.add(new ArrayList<>(p));
-            return list;
+        List<List<Integer>> result = new ArrayList<>();
+        result.add(new ArrayList<>());
+        for(int n:nums){
+            int s = result.size();
+            for(int i=0;i<=s-1;i++){
+                List<Integer> list = new ArrayList<>(result.get(i));
+                list.add(n);
+                result.add(list);
+            }
         }
-        int num = up[0];
-        int[] remaining = new int[up.length-1];
-        int idx = 0;
-        for(int i=1;i<=up.length-1;i++){
-            remaining[idx++] = up[i];
-        }
-        p.add(num);
-        List<List<Integer>> left = subset(p,remaining);
-        p.remove(p.size()-1);
-        List<List<Integer>> right = subset(p,remaining);
-        left.addAll(right);
-        return left;
+        return result;
     }
 }
