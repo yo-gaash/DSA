6c4de532138156646f39f21b6a69086c199aa706
YOGASH S <159635469+yo-gaash@users.noreply.github.com>
2026-08-18 10:09:07 +0530

    feat: Add Subsets

--- LeetCode/Array/Subsets/java/Solution.java
+++ LeetCode/Array/Subsets/java/Solution.java
@@ -1,15 +1,25 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        recurse(0,nums,result,new ArrayList<>());
-        return result;
+        List<List<Integer>> ans = subset(new ArrayList<>(), nums);
+        return ans;
     }
-    public static void recurse(int start, int[] nums, List<List<Integer>> result,List<Integer> curr){
-        result.add(new ArrayList<>(curr));
-        for(int i=start;i<=nums.length-1;i++){
-            curr.add(nums[i]);
-            recurse(i+1,nums,result,curr);
-            curr.remove(curr.size()-1);
+    public static List<List<Integer>> subset(List<Integer> p, int[] up){
+        if(up.length==0){
+            List<List<Integer>> list = new ArrayList<>();
+            list.add(new ArrayList<>(p));
+            return list;
         }
+        int num = up[0];
+        int[] remaining = new int[up.length-1];
+        int idx = 0;
+        for(int i=1;i<=up.length-1;i++){
+            remaining[idx++] = up[i];
+        }
+        p.add(num);
+        List<List<Integer>> left = subset(p,remaining);
+        p.remove(p.size()-1);
+        List<List<Integer>> right = subset(p,remaining);
+        left.addAll(right);
+        return left;
     }
 }
