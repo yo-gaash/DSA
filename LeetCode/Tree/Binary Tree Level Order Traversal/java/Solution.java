f63fe1c5c6f45440cb2dc9dad9d82c8e005d5bf4
YOGASH S <159635469+yo-gaash@users.noreply.github.com>
2026-08-21 10:38:28 +0530

    feat: Add Binary Tree Level Order Traversal

--- LeetCode/Tree/Binary Tree Level Order Traversal/java/Solution.java
+++ LeetCode/Tree/Binary Tree Level Order Traversal/java/Solution.java
@@ -15,27 +15,27 @@
  */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> list = new ArrayList<>();
+        List<List<Integer>> result = new ArrayList<>();
         if(root==null){
-            return list;
+            return result;
         }
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(root);
-        while(!queue.isEmpty()){
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        while(!q.isEmpty()){
+            int size = q.size();
             List<Integer> temp = new ArrayList<>();
-            int size = queue.size();
             for(int i=0;i<=size-1;i++){
-                TreeNode current = queue.poll();
+                TreeNode current = q.poll();
                 temp.add(current.val);
                 if(current.left!=null){
-                    queue.add(current.left);
+                    q.offer(current.left);
                 }
                 if(current.right!=null){
-                    queue.add(current.right);
+                    q.offer(current.right);
                 }
             }
-            list.add(new ArrayList<>(temp));
+            result.add(new ArrayList<>(temp));
         }
-        return list;
+        return result;
     }
 }
