3d66beed7647f7dc76ea79fb07eb250c84b2ab81
YOGASH S <159635469+yo-gaash@users.noreply.github.com>
2026-08-25 12:30:24 +0530

    feat: Add First Bad Version

--- LeetCode/Binary Search/First Bad Version/java/Solution.java
+++ LeetCode/Binary Search/First Bad Version/java/Solution.java
@@ -3,18 +3,24 @@
 
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
-        int left = 1, right = n;
-        
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-            
-            if (isBadVersion(mid)) {
-                right = mid; // mid might be the first bad version
-            } else {
-                left = mid + 1; // move right
+        return binarySearch(1,n);
+    }
+    public  int binarySearch(int start, int end){
+        while(start<=end){
+            int mid = start+((end-start)/2);
+            if(isBadVersion(1)){
+                return 1;
+            }
+            if(isBadVersion(mid)){
+                if(!isBadVersion(mid-1)){
+                    return mid;
+                }
+                end = mid-1;
+            }
+            else{
+                start = mid+1;
             }
         }
-        
-        return left; // or right, both are the first bad version  
+        return -1;
     }
 }
